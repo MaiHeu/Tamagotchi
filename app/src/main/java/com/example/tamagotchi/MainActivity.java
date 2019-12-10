ae62db3bcd92e967670b28bfc7eca986136896a8
Max Ceb <hopemili@yahoo.com>
2019-12-10 11:40:10 +0100

    Added icons

--- app/src/main/java/com/example/tamagotchi/MainActivity.java
+++ app/src/main/java/com/example/tamagotchi/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Hallo HAmster");
+        System.out.println("Hallo Hamster");
     }
 }
