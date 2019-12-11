7bc8aa6e0425be4b8e3bbe249b6df793829bc67d
Max Ceb <hopemili@yahoo.com>
2019-12-11 13:48:39 +0100

    Button Play leads to createActivity

--- app/src/main/java/com/example/tamagotchi/MainActivity.java
+++ app/src/main/java/com/example/tamagotchi/MainActivity.java
@@ -2,14 +2,27 @@ package com.example.tamagotchi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonPlay;
+
+    public void onClick_buttonPlay(View v)
+    {
+        Intent intent = new Intent(MainActivity.this, createActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("Hallo Hamster");
+
+        buttonPlay = findViewById(R.id.buttonPlay);
     }
 }
